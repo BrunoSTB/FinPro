5d8d97d19b31b58d1e3364242b59e58e84395ba5
Bruno Braga <brunobragast@gmail.com>
2024-06-22 00:19:56 -0300

    updated import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopNavbarComponent } from './top-navbar/top-navbar.component';
 import { ExpensesCardComponent } from './expenses-card/expenses-card.component';
-import { ExpensesDashboardComponent } from './expenses/expenses-dashboard.component';
+import { ExpensesDashboardComponent } from './expenses-dashboard/expenses-dashboard.component';
 
 @Component({
   selector: 'app-root',
